795f3faeb86b003eb84840c48f81cdc8c6c769e3
michaeljstevens <michaeljstevens@zoho.com>
2016-12-04 22:24:32 -0800

    component renders

---
+++ frontend/components/root.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+  render() {
+    return(
+      <div>
+        root component
+      </div>
+    );
+  }
+}
+
+export default Root;
